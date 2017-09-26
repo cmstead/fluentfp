eac6949a08173a83a8dc8ec249b3c019b0503b5e
Chris Stead <cmstead@gmail.com>
2017-09-25 21:49:03 -0700

    Added curry, apply and identity

--- bin/core-functions.js
+++ bin/core-functions.js
@@ -3,14 +3,76 @@
 
     if (isNode) {
         const signet = require('./signet-types');
+        const coreMonads = require('./core-monads');
 
-        module.exports = moduleFactory(signet);
+        module.exports = moduleFactory(signet, coreMonads);
     } else {
-        window.coreFunctions = moduleFactory(signet);
+        window.coreFunctions = moduleFactory(signet, window.coreMonads);
     }
 
-})(function (signet) {
+})(function (signet, coreMonads) {
     'use strict';
 
-    (signet);
+    function identity(value) {
+        return value;
+    }
+
+    function apply (fn, args) {
+        return fn.apply(null, args);
+    }
+
+    const slice = (start, end) => (values) => {
+        const endIndex = coreMonads.either('int', values.length)(end);
+        const result = [];
+
+        for(var i = start; i < endIndex; i++) {
+            result.push(values[i]);
+        }
+
+        return result;
+    };
+
+    function concat (destination, source) {
+        let result = slice(0)(destination);
+
+        for (let i = 0; i < source.length; i++) {
+            result.push(source[i]);
+        }
+
+        return result
+    }
+
+    function setLength (fn, length) {
+        return Object.defineProperty(fn, 'length', {
+            writeable: false,
+            configurable: true,
+            value: length
+        });
+    }
+
+    function functionWrapper(fn, args) {
+        function curriedFn () {
+            const finalArgs = concat(args, slice(0)(arguments));
+            return finalArgs.length >= fn.length ? apply(fn, finalArgs) : functionWrapper(fn, finalArgs);
+        }
+
+        return setLength(curriedFn, fn.length - args.length);
+    }
+
+    function curry (fn) {
+        const args = slice(1)(arguments);
+        return functionWrapper(fn, args);
+    }
+
+    return {
+        apply: signet.enforce(
+            'fn:function, args:variant<array, arguments> => *',
+            apply),
+        curry: signet.enforce(
+            'fn:function, args...:* => curriedFunction:function',
+            curry),
+        identity: signet.enforce(
+            'x:* => *',
+            identity)
+    };
 });
--- test/core-functions.test.js
+++ test/core-functions.test.js
@@ -10,8 +10,56 @@ const sinon = require('sinon');
 
 const coreFunctions = require('../bin/core-functions.js');
 
+function add (a, b) { return a + b; }
+
 describe('core-functions', function () {
     require('./test-utils/approvals-config');
+
+    describe('apply', function() {
+        
+        it('should apply arguments to a passed function', function() {
+
+            assert.equal(coreFunctions.apply(add, [5, 6]), 11);
+        });
+
+    });
+
+    describe('identity', function() {
+        
+        it('should return the argument which is passed', function() {
+            assert.equal(coreFunctions.identity('testing'), 'testing');
+        });
+
+    });
+
+    describe('curry', function() {
+
+        it('should curry a one-argument function', function() {
+            var curriedIdentity = coreFunctions.curry(coreFunctions.identity);
+            assert.equal(curriedIdentity()(1234), 1234);
+        });
+
+        it('should curry a multi-value function', function() {
+            var curriedAdd = coreFunctions.curry(add);
+            assert.equal(curriedAdd(5)(6), 11);
+        });
+
+        it('should not share state', function() {
+            const curriedAdd = coreFunctions.curry(add);
+            const add1 = curriedAdd(1);
+            const add2 = curriedAdd(2);
+
+            assert.equal(add1(2), 3);
+            assert.equal(add2(3), 5);
+        });
+
+        it('should update function length appropriately when currying', function() {
+            const curriedAdd = coreFunctions.curry(add);
+            assert.equal(curriedAdd(1).length, 1);
+        });
+
+    });
+
 });
 
 if (typeof global.runQuokkaMochaBdd === 'function') {
